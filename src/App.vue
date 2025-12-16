e6654e86cb315e1df4c05dd9a0cd37976f58e36b
Userzt <user@example.com>
2025-12-16 21:18:10 +0800

    Initial commit: Vue3 + Ant Design Vue + Vite project setup

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header>
+        <div class="logo">Vue 3 + Ant Design</div>
+        <a-menu
+          theme="dark"
+          mode="horizontal"
+          :default-selected-keys="['1']"
+          style="line-height: 64px"
+        >
+          <a-menu-item key="1">
+            <router-link to="/">首页</router-link>
+          </a-menu-item>
+          <a-menu-item key="2">
+            <router-link to="/about">关于</router-link>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-header>
+      
+      <a-layout-content style="padding: 24px">
+        <router-view />
+      </a-layout-content>
+      
+      <a-layout-footer style="text-align: center">
+        Vue 3 + Ant Design Vue + Vite ©2024
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+.logo {
+  float: left;
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+  margin-right: 24px;
+}
+
+.ant-menu-horizontal {
+  border-bottom: none;
+}
+
+.ant-menu-item a {
+  color: rgba(255, 255, 255, 0.65);
+}
+
+.ant-menu-item-selected a {
+  color: #1890ff;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <a-row :gutter="24">
+      <a-col :span="24">
+        <a-card title="欢迎使用 Vue 3 + Ant Design Vue">
+          <p>这是一个基于 Vue 3、Ant Design Vue 和 Vite 构建的项目框架。</p>
+          
+          <a-divider />
+          
+          <a-space direction="vertical" size="middle" style="width: 100%">
+            <a-alert
+              message="项目特性"
+              description="Vue 3 Composition API、Ant Design Vue 组件库、Vite 快速构建、History 路由模式"
+              type="info"
+              show-icon
+            />
+            
+            <a-button type="primary" @click="showMessage">
+              <template #icon>
+                <CheckCircleOutlined />
+              </template>
+              点击测试
+            </a-button>
+          </a-space>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script>
+import { message } from 'ant-design-vue'
+import { CheckCircleOutlined } from '@ant-design/icons-vue'
+
+export default {
+  name: 'Home',
+  components: {
+    CheckCircleOutlined
+  },
+  setup() {
+    const showMessage = () => {
+      message.success('Vue 3 + Ant Design Vue 运行正常！')
+    }
+
+    return {
+      showMessage
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  padding: 20px;
+}
+</style>
